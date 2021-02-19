15696b5940566a57461fcf7c9a55664a7bf92550
ccha23 <chung.chan@cityu.edu.hk>
2021-02-20 01:03:39 +0800

    Tutorial6 dump

--- Tutorial6/cache/joblib/__main__--home-course-cs5483-nbgrader-source-Tutorial6-<ipython-input-caad8ff4a835>/bagging/func_code.py
+++
@@ -1,9 +0,0 @@
-# first line: 1
-@memory.cache
-def bagging(n_estimators, max_depth):
-    BAG = ensemble.BaggingClassifier(
-        base_estimator=tree.DecisionTreeClassifier(max_depth=max_depth),
-        n_estimators=n_estimators,
-        random_state=0)
-    BAG.fit(X_train, Y_train)
-    return BAG.score(X_test, Y_test)
